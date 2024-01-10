48bf0dd0a505d237e40988a80d9f5ccc82996285
Tamir <tamir.gluzman@post.runi.ac.il>
2024-01-10 20:11:50 +0200

    Finished code

--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -22,21 +24,97 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        // System.out.println(capVowelsLowRest("Hello World"));
+        // System.out.println(capVowelsLowRest("One two tHRee world"));
+        // System.out.println(capVowelsLowRest("vowels are fun") );
+        // System.out.println(capVowelsLowRest("intro"));
+        // System.out.println(capVowelsLowRest("yellow"));
+
+        // System.out.println(camelCase(("  hello world   hello ")));
+
+        // System.out.println(Arrays.toString(allIndexOf("MMMM",'M')));
+    }
+
+     public static char toLower (char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char) (c + 32);
+        }
+        else if (c >= 'a' && c <= 'z') {
+            return c;
+        }
+        return '0';
+    }
+
+    public static char toUpper (char c) {
+        if (c >= 'a' && c <= 'z') {
+            return (char) (c - 32);
+        }
+        else if (c >= 'A' && c <= 'Z') {
+            return c;
+        }
+        return '0';
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String capVowelsLowRest = "";
+        String VOWELS = "aeiouAEIOU";
+        String CONSONANTS ="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+        for (int i = 0; i < string.length(); i++) {
+            if (VOWELS.indexOf(string.charAt(i)) != -1) {
+                capVowelsLowRest += toUpper(string.charAt(i));
+            }
+            else if (CONSONANTS.indexOf(string.charAt(i)) != -1) {
+                capVowelsLowRest += toLower(string.charAt(i));
+            }
+            else {
+                capVowelsLowRest += string.charAt(i);
+            }
+        }
+        return capVowelsLowRest;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String camelCase = "";
+
+        for (int i = 0; i < string.length(); i++) {
+            if (i == 0) {
+                if (string.charAt(i) != ' ') {
+                    camelCase += toLower(string.charAt(i));
+                }
+            }
+            else if (string.charAt(i) != ' ') {
+                if (string.charAt(i - 1) == ' ' && camelCase.length() > 0) {
+                    camelCase += toUpper(string.charAt(i));
+                }
+                else {
+                    camelCase += toLower(string.charAt(i));
+                }
+            }
+        }
+
+        return camelCase;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int chrCount = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                chrCount++;
+            }
+        }
+
+        int[] chrIndexes = new int[chrCount];
+        
+        int j = 0;
+        for (int i = 0; i < string.length(); i++) {
+             if (string.charAt(i) == chr) {
+                chrIndexes[j] = string.substring(i).indexOf(chr) + i;
+                j++;
+            }
+            
+        }
+
+        return chrIndexes;
     }
 }
